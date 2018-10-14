3e4088300d1ea1cb55126eec0973d4bc2c385b64
Suru <tou473.vang@hotmail.com>
2018-10-14 04:39:29 -0500

    New command s.myprofilepic
    This reverts commit 1a9ac1466220c96ffefe792bfb0a7007f756b38a.

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ bot.on('message', msg =>{
 
          Normal Commands
          s.lolfind <username> - Looks up the username on op.gg. (NA Players)
-         s.whosyourdaddy - Tells you who my daddy is.
-         s.myprofilepic - Posts a link to your discord profile picture.\`\`\``);
+         s.whosyourdaddy - Tells you who my daddy is.\`\`\``);
 
          if (msg.member.permissions.has('ADMINISTRATOR')){
              msg.member.send(`\`\`\`You are recieving this secondary list because you have administrative permissions:
@@ -71,12 +70,9 @@ bot.on('message', msg =>{
     }
 
     if (command === 'whosyourdaddy'){
-        msg.channel.send('I was created by a programmer called Suru.');
+        msg.channel.send('I was created by Suru#0267.');
     }
 
-    if (command === 'myprofilepic'){
-        msg.channel.send(`Here's your profile picture: ${msg.author.avatarURL}`);
-    }
     // Works but needs a reason to use
     // if (command === 'postImage'){
     //     const attachment = new Attachment('');
